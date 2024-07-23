b2cd157377d977ac8d3df1dd9c96cf2df07fad31
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-23 11:07:37 -0500

    stitcher updated

--- modules/image_stitcher/stitcher.py
+++ modules/image_stitcher/stitcher.py
@@ -22,4 +22,4 @@ def recursive_stitching(images, out_dir, stitching_factor = 32, iteration_factor
             rec_images.append(stitched_image)
             cv2.imwrite(f"{out_dir}/iteration_{str(epoch).zfill(2)}_image: {str(i).zfill(4)}:{str(i+stitching_factor).zfill(4)}.png", stitched_image)
             print(f"Iteration: {epoch}\tImage: {i}:{i+stitching_factor}")
-    return recursive_stitching(rec_images, stitching_factor=3, iteration_factor=2, epoch=epoch+1)
+    return recursive_stitching(rec_images, out_dir=out_dir, stitching_factor=3, iteration_factor=2, epoch=epoch+1)
